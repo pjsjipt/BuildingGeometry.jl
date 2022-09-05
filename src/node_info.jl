5982d70f0372b18a41c79c71a24a82e721da41d3
= <pjabardo@ipt.br>
2022-09-05 11:36:53 -0300

    Implemented `NodeInfo`
    This will be used to specify information on external/internal pressure
    on each node (element or vertex)

---
+++ src/node_info.jl
@@ -0,0 +1,29 @@
+
+"""
+`NodeInfo(A,iex,iin)`
+
+Represents node information. In this context, node information
+is information on outer and inner pressure tap.
+
+In the simplest case, the external node is the id of the external pressure
+tap and inner node the id of internal pressure tap. Often the internal
+pressure is not measured and some other mean of calculation is used.
+
+The pressure information can refer to an element of surface or a node.
+So to calculate forces and moments, the contribution of this node is given
+by the area times the normal
+"""
+struct NodeInfo{Dim,T,Tex,Tin,TRI}
+    "Area times outward normal"
+    A::Vec{Dim,T}
+    "External node"
+    iex::Tex
+    "Internal node"
+    iin::Tin
+    "Triangle/mesh"
+    tri::TRI
+end
+
+
+
+    
