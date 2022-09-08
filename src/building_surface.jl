e51b6442c0a3039586a365d539b9a100c7587d2c
= <pjabardo@ipt.br>
2022-09-08 11:50:21 -0300

    Everything appears to be working.
    I still need to test harder cases but I am almost done...

--- src/building_surface.jl
+++ src/building_surface.jl
@@ -1,9 +1,11 @@
 
 import StaticArrays: SVector
+import Meshes: connect, SimpleMesh
 
-struct BuildingSurface{T,Tex,Tin}
-    msh::Vector{Triangle{3,T,SVector{3,Point{3,T}}}}
+struct BuildingSurface{T,Tex,Tin,TMesh}
+    tri::Vector{Triangle{3,T,SVector{3,Point{3,T}}}}
     nodes::Vector{NodeInfo{3,T,Tex,Tin,Int}}
+    mesh::TMesh
 end
 
 
@@ -22,7 +24,7 @@ function buildsurface(cad::AbstractVector{<:Triangle{3,Float64}},
     
     # The external and internal sides should span the entire surface.
     # Different internal surfaces shouldn't overlap
-
+    
     # Let's deal with external surface:
     esec = sections[1]
     if !haskey(esec, :points)
@@ -31,10 +33,11 @@ function buildsurface(cad::AbstractVector{<:Triangle{3,Float64}},
         eid = [esec.id[1]]
         eidx = [esec.tri]
     else
+        (length(esec.id) != length(esec.points)) &&
+            error("`id` should have the same length as `points`")
         evor, eidx = discrsurface(cad, esec.tri, esec.points)
         eid = esec.id
     end
-    
     if nsecs == 1  # No internal surfaces
         for (i, m) in enumerate(evor)
             for (k,t) in enumerate(m)
@@ -43,51 +46,65 @@ function buildsurface(cad::AbstractVector{<:Triangle{3,Float64}},
                 push!(nodes, NodeInfo(An, eid[i], nointid, eidx[i][k]))
             end
         end
-        return BuildingSurface(msh, nodes)
-    end
+    else
 
-    Ne = length(evor)
-    
-    # Let's deal with internal faces
-    itri = [collect(sections[i].tri) for i in 2:nsecs]
-    etri = esec.tri
-    # There should be no overlap
-    nisecs = nsecs-1
-    for i in 1:nisecs-1
-        for k in i+1:nsecs
-            if length(itri[i] ∩ itri[k]) > 0
-                error("There should be no overlap in different sections of internal surfaces!")
+        Ne = length(evor)
+        
+        # Let's deal with internal faces
+        itri = [collect(sections[i].tri) for i in 2:nsecs]
+        etri = esec.tri
+        # There should be no overlap
+        nisecs = nsecs-1
+        for i in 1:nisecs-1
+            for k in i+1:nisecs
+                if length(itri[i] ∩ itri[k]) > 0
+                    error("There should be no overlap in different sections of internal surfaces!")
+                end
             end
         end
-    end
-    itri1 = sort(vcat(itri...))
-    etri1 = sort(etri)
-
-    if itri1 != etri1
-        error("Internal and external surfaces should be the same")
-    end
-    
-    # Let's start building the mesh
-    for sec in sections[begin+1:end]
-        if !haskey(sec, :points) # No need for voronoi. Just add the triangles
-            ivor = [cad[sec.tri]]
-            iid = [sec.id[1]]
-            iidx = [sec.tri]
-        else
-            ivor, iidx = discrsurface(cad, sec.tri, sec.points)
-            iid = sec.id
-        end
-        Ni = length(ivor)
-        for e in 1:Ne
-            for i in 1:Ni
-                intersectmesh!(msh, nodes, eid[e], evor[e], eidx[e],
-                               iid[i], ivor[i], iidx[i])
-            end
+        itri1 = sort(vcat(itri...))
+        etri1 = sort(etri)
+        
+        if itri1 != etri1
+            error("Internal and external surfaces should be the same")
         end
         
+        # Let's start building the mesh
+        for sec in sections[begin+1:end]
+            if !haskey(sec, :points) # No need for voronoi. Just add the triangles
+                ivor = [cad[sec.tri]]
+                iid = [sec.id[1]]
+                iidx = [sec.tri]
+            else
+                ivor, iidx = discrsurface(cad, sec.tri, sec.points)
+                iid = sec.id
+            end
+            Ni = length(ivor)
+            for e in 1:Ne
+                for i in 1:Ni
+                    intersectmesh!(msh, nodes, eid[e], evor[e], eidx[e],
+                                   iid[i], ivor[i], iidx[i])
+                end
+            end
+            
+        end
     end
     
-    return BuildingSurface(msh, nodes)
+    # Lets get the vertices
+    ntri = length(msh)
+    verts = fill(Point{3,Float64}(0,0,0), ntri * 3)
+    conn = fill((0,0,0), ntri)
+    for i in 1:ntri
+        k = (i-1)*3 + 1
+        vtri = vertices(msh[i])
+        verts[k] = vtri[1]
+        verts[k+1] = vtri[2]
+        verts[k+2] = vtri[3]
+        conn[i] = (k,k+1,k+2)
+    end
+    mesh = SimpleMesh(verts, connect.(conn, Triangle))
+    
+    return BuildingSurface(msh, nodes, mesh)
     
 end
 
--- test/test_buildsurface.jl
+++ test/test_buildsurface.jl
@@ -1,7 +1,84 @@
 
 
 let
+    # Basic stuff. A single external surface
+    pts = Point.([(-0.5,-0.5,0),(0.5,-0.5,0),(-0.5,0.5,0),(0.5,0.5,0)])
+    tri = [Triangle((-1,-1,0), (1,-1,0), (1,1,0)), Triangle((-1,-1,0),(1,1,0),(-1,1,0))]
+    m =  buildsurface(tri, [(points=pts, tri=1:2, id=1:4)])
+    Ne = length(pts)
+    Ae = zeros(Ne)
+    for n in m.nodes
+        Ae[n.iex] += norm(n.A)
+    end
+    @test sum(Ae) ≈ 4.0
+    @test all(Ae .≈ 1.0)
 
+
+    ipts = Point.([(-0.75, 0,0), (0.75, 0,0)])
+    m =  buildsurface(tri, [(points=pts, tri=1:2, id=1:4),
+                            (points=ipts, tri=1:2, id=5:6)])
+    Ne = length(pts)
+    Ni = length(ipts)
+
+    Ae = zeros(Ne)
+    Ai = zeros(Ni)
+    
+    for n in m.nodes
+        A = norm(n.A)
+        Ae[n.iex] += A
+        Ai[n.iin-4] += A
+    end
+    @test sum(Ae) ≈ 4.0
+    @test sum(Ai) ≈ 4.0
+    @test all(Ae .≈ 1.0)
+    @test all(Ai .≈ 2.0)
+
+    # Lets try something more complex
+    tri = [Triangle((0,0,0), (1,0,0), (1,1,0)), Triangle((0,0,0), (1,1,0), (0,1,0))]
+    Ne = 10
+    Ni = 5
+    epts = Point.(rand(Ne), rand(Ne), 0)
+    ipts = Point.(rand(Ni), rand(Ni), 0)
+    m =  buildsurface(tri, [(points=epts, tri=1:2, id=1:Ne),
+                            (points=ipts, tri=1:2, id=1:Ni)])
+    
+    Ae = zeros(Ne)
+    Ai = zeros(Ni)
+    
+    for n in m.nodes
+        A = norm(n.A)
+        Ae[n.iex] += A
+        Ai[n.iin] += A
+    end
+    @test sum(Ae) ≈ 1.0
+    @test sum(Ai) ≈ 1.0
+    
+    
+    tri = [Triangle((0.,0.,0.), (1.,0.,0.), (1.,0.75,0.)),
+              Triangle((0.,0.,0.), (1.,0.75,0.), (0.,0.75,0.)),
+              Triangle((0.,0.75,0.), (1.,0.75,0.), (1.,1.,0.)),
+              Triangle((0.,0.75,0.), (1.,1.,0.), (0.,1.,0.))]
+
+    Ne = 50
+    Ni = 20
+    epts = Point.(rand(Ne), rand(Ne), 0)
+    ipts = Point.(0.75*rand(Ni), 0.75*rand(Ni), 0)
+    m =  buildsurface(tri, [(points=epts, tri=1:4, id=1:Ne),
+                            (points=ipts, tri=1:2, id=1:Ni),
+                            (tri=3:4, id=-1)])
+    
+    
+    Ae = zeros(Ne)
+    Ai = zeros(Ni)
+    
+    for n in m.nodes
+        A = norm(n.A)
+        Ae[n.iex] += A
+        n.iin > 0 && (Ai[n.iin] += A)
+    end
+    @test sum(Ae) ≈ 1.0
+    @test sum(Ai) ≈ 0.75
+    
     pts = Point.([(-0.5,-0.5,0),(0.5,-0.5,0),(-0.5,0.5,0),(0.5,0.5,0)])
     tri = [Triangle((-1,-1,0), (1,-1,0), (1,1,0)), Triangle((-1,-1,0),(1,1,0),(-1,1,0))]
 
